116ccf4d4216b08f760b0c7556ece33ab01de8fb
asim1 <asim1@DESKTOP-MO5BN54>
2021-07-16 01:28:08 +0100

    test

---
+++ fyp/src/test/java/com/asim/fyp/FypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asim.fyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
